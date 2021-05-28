10fa5a53f9d1f96f88442a3197f327c5e0b5010b
jhj5897 <wkdgpwls1@naver.com>
2021-05-28 17:57:16 +0900

    no message

--- ForDeepSleep/app/src/main/java/org/jhj/fordeepsleep/AlarmListActivity.kt
+++ ForDeepSleep/app/src/main/java/org/jhj/fordeepsleep/AlarmListActivity.kt
@@ -2,10 +2,20 @@ package org.jhj.fordeepsleep
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jhj.fordeepsleep.databinding.ActivityAlarmListBinding
 
 class AlarmListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAlarmListBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_alarm_list)
+        binding = ActivityAlarmListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var toolbar = binding.toolbar
+
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back)
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
     }
 }
--- ForDeepSleep/app/src/main/java/org/jhj/fordeepsleep/MainActivity.kt
+++ ForDeepSleep/app/src/main/java/org/jhj/fordeepsleep/MainActivity.kt
@@ -1,5 +1,6 @@
 package org.jhj.fordeepsleep
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.jhj.fordeepsleep.databinding.ActivityMainBinding
@@ -12,6 +13,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var toolbar = binding.toolbar
 
+        toolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_exist_alarm -> {
+                    var intent = Intent(this, AlarmListActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
     }
+
+
 }
