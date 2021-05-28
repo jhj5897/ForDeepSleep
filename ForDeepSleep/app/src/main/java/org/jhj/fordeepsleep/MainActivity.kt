92e01060193050cdd7fa4b1d591fd76817ec8d59
jhj5897 <wkdgpwls1@naver.com>
2021-05-28 13:53:10 +0900

    setting before development.

---
+++ ForDeepSleep/app/src/main/java/org/jhj/fordeepsleep/MainActivity.kt
@@ -0,0 +1,17 @@
+package org.jhj.fordeepsleep
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.jhj.fordeepsleep.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
