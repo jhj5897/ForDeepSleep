fbf9e1bcee3786854340dee4e188e193eda18b5b
jhj5897 <wkdgpwls1@naver.com>
2021-05-28 17:56:38 +0900

    create main page layout.

---
+++ ForDeepSleep/app/src/main/java/org/jhj/fordeepsleep/AlarmListActivity.kt
@@ -0,0 +1,11 @@
+package org.jhj.fordeepsleep
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlarmListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm_list)
+    }
+}
